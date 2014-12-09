1a6c4502ee2f2794cc42f19f0edd4a174f1eb500
Joe Fallon <joseph.t.fallon@gmail.com>
2014-12-09 10:40:18 -0800

    The URL class was removed from the project.

--- tests/index.php
+++ tests/index.php
@@ -8,6 +8,5 @@ new \tests\JoeFallon\PhpHttp\AgentTests();
 new \tests\JoeFallon\PhpHttp\HttpCodesTests();
 new \tests\JoeFallon\PhpHttp\MimeTypeTests();
 new \tests\JoeFallon\PhpHttp\PageRedirectTests();
-new \tests\JoeFallon\PhpHttp\UrlTests();
 
 UnitTest::getAllUnitTestsSummary();
